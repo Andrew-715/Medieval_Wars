93c9f52023fa3201d77a7b961809fde0f5f961cf
Andrew-715 <kutuzkin98@mail.ru>
2023-03-06 13:35:37 +0800

    v. 1.0.0

---
+++ equipment.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from random import uniform
+from typing import Optional
+
+import marshmallow_dataclass
+import marshmallow
+import json
+
+
+@dataclass
+class Armor:
+    id: int
+    name: str
+    defence: float
+    stamina_per_turn: float
+
+
+@dataclass
+class Weapon:
+    id: int
+    name: str
+    min_damage: float
+    max_damage: float
+    stamina_per_hit: float
+
+    @property
+    def damage(self) -> float:
+        return round(uniform(self.min_damage, self.max_damage), 1)
+
+
+@dataclass
+class EquipmentData:
+    weapons: list[Weapon]
+    armors: list[Armor]
+
+
+class Equipment:
+
+    def __init__(self):
+        self.equipment = self._get_equipment_data()
+
+    def get_weapon(self, weapon_name: str) -> Optional[Weapon]:
+        for weapon in self.equipment.weapons:
+            if weapon.name == weapon_name:
+                return weapon
+        raise NotImplementedError
+
+    def get_armor(self, armor_name: str) -> Optional[Armor]:
+        for armor in self.equipment.armors:
+            if armor.name == armor_name:
+                return armor
+        raise NotImplementedError
+
+    def get_weapons_names(self) -> list:
+        return [weapon.name for weapon in self.equipment.weapons]
+
+    def get_armors_names(self) -> list:
+        return [armor.name for armor in self.equipment.armors]
+
+    @staticmethod
+    def _get_equipment_data() -> EquipmentData:
+        with open("./data/equipment.json", encoding='utf-8') as file:
+            data = json.load(file)
+            equipment_schema = marshmallow_dataclass.class_schema(EquipmentData)
+            try:
+                return equipment_schema().load(data)
+            except marshmallow.exceptions.ValidationError:
+                raise ValueError
